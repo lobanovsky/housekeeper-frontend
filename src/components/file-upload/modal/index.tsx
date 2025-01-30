55077c13f9c8c4755588af75f60bcee4a47ac6be
Холина Дарья Анатольевна <holina.darya@otr.ru>
2025-01-30 17:35:06 +0300

    Фикс сборки v4

--- src/components/file-upload/modal/index.tsx
+++ src/components/file-upload/modal/index.tsx
@@ -1,19 +1,14 @@
 // @ts-nocheck
 import React from 'react';
-import { Modal, ModalProps } from 'antd';
-import { FileUploadProps } from 'components/file-upload/types';
+import { Modal } from 'antd';
 import { FileUpload } from '../index';
-
-interface FileUploadModalProps extends Omit<FileUploadProps, 'closeModal' | 'width'>, ModalProps {
-  width?: number;
-}
 
 export const showFileUploadModal = ({
                                       title,
                                       width = 800,
                                       className = '',
                                       ...uploadProps
-                                    }: FileUploadModalProps) => {
+                                    }: any) => {
   let modal: any = null;
 
   const closeModal = () => {
